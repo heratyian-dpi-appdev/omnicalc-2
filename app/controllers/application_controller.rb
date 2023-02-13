8d754ff25bcb9d59abf1ee2c18d2c9c043152d61
Ian <heratyian@gmail.com>
2023-02-13 21:44:39 +0000

    Hook up subtract form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,16 @@ class ApplicationController < ActionController::Base
 
     render({ template: "calculation_templates/add_results" })
   end
+
+  def subtract
+    render({ template: "calculation_templates/subtract_form" })
+  end
+
+  def subtract_wizard
+    @first = params.fetch("first-input").to_f
+    @second = params.fetch("second-input").to_f
+    @result = @second - @first
+
+    render({ template: "calculation_templates/subtract_results" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get("/add", { controller: "application", action: "add" })
+  get("/add_wizard", { controller: "application", action: "add_wizard" })
 
-  get('/add', { controller: 'application', action: 'add' })
-  get('/add_wizard', { controller: 'application', action: 'add_wizard' })
+  get("/subtract", { controller: "application", action: "subtract" })
+  get("/subtract_wizard", { controller: "application", action: "subtract_wizard" })
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
