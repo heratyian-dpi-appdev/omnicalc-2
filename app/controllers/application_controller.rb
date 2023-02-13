81790f06b4bb177c3e243799821cde04d06f4d67
Ian <heratyian@gmail.com>
2023-02-13 21:33:55 +0000

    Hook up /add route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  def add
+    render({ template: "calculation_templates/add_form" })
+  end
 
+  def add_wizard
+    @first = params.fetch("first-input").to_f
+    @second = params.fetch("second-input").to_f
+    @result = @first + @second
+
+    render({ template: "calculation_templates/add_results" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
+  get('/add', { controller: 'application', action: 'add' })
+  get('/add_wizard', { controller: 'application', action: 'add_wizard' })
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
